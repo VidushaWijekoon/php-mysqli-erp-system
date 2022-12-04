7119b2cc131896b89444d1cc90e5116eec8f8dd3
vidusha1 <vidisha@alsakbcomputer.com>
2022-12-04 11:59:50 +0400

    header and footer and add fontawesome pro version

--- presentation/includes/footer.php
+++ presentation/includes/footer.php
@@ -16,27 +16,8 @@
        <!-- jQuery UI 1.11.4 -->
        <script src="../../static/plugins/jquery-ui/jquery-ui.min.js"></script>
        <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
-       <script>
-$.widget.bridge('uibutton', $.ui.button)
-       </script>
        <!-- Bootstrap 4 -->
        <script src="../../static/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-       <!-- ChartJS -->
-       <script src="../../static/plugins/chart.js/Chart.min.js"></script>
-       <!-- Sparkline -->
-       <script src="../../static/plugins/sparklines/sparkline.js"></script>
-       <!-- JQVMap -->
-       <script src="../../static/plugins/jqvmap/jquery.vmap.min.js"></script>
-       <script src="../../static/plugins/jqvmap/maps/jquery.vmap.usa.js"></script>
-       <!-- jQuery Knob Chart -->
-       <script src="../../static/plugins/jquery-knob/jquery.knob.min.js"></script>
-       <!-- daterangepicker -->
-       <script src="../../static/plugins/moment/moment.min.js"></script>
-       <script src="../../static/plugins/daterangepicker/daterangepicker.js"></script>
-       <!-- Tempusdominus Bootstrap 4 -->
-       <script src="../../static/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
-       <!-- Summernote -->
-       <script src="../../static/plugins/summernote/summernote-bs4.min.js"></script>
        <!-- overlayScrollbars -->
        <script src="../../static/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
        <!-- AdminLTE App -->
@@ -47,10 +28,6 @@ $.widget.bridge('uibutton', $.ui.button)
        <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
        <script src="../../static/dist/js/pages/dashboard2.js"></script>
        <!-- jQuery Mapael -->
-       <script src="../../static/plugins/jquery-mousewheel/jquery.mousewheel.js"></script>
-       <script src="../../static/plugins/raphael/raphael.min.js"></script>
-       <script src="../../static/plugins/jquery-mapael/jquery.mapael.min.js"></script>
-       <script src="../../static/plugins/jquery-mapael/maps/usa_states.min.js"></script>
        <script src="../../plugins/jquery/jquery.min.js"></script>
        <!-- DataTables  & Plugins -->
        <script src="../../static/plugins/datatables/jquery.dataTables.min.js"></script>
@@ -59,9 +36,7 @@ $.widget.bridge('uibutton', $.ui.button)
        <script src="../../static/plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
        <script src="../../static/plugins/datatables-buttons/js/dataTables.buttons.min.js"></script>
        <script src="../../static/plugins/datatables-buttons/js/buttons.bootstrap4.min.js"></script>
-       <script src="../../static/plugins/jszip/jszip.min.js"></script>
-       <script src="../../static/plugins/pdfmake/pdfmake.min.js"></script>
-       <script src="../../static/plugins/pdfmake/vfs_fonts.js"></script>
+
        <script src="../../static/plugins/datatables-buttons/js/buttons.html5.min.js"></script>
        <script src="../../static/plugins/datatables-buttons/js/buttons.print.min.js"></script>
        <script src="../../static/plugins/datatables-buttons/js/buttons.colVis.min.js"></script>
